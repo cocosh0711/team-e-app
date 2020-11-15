a7f7397b0656162e60bd7a300af609a438c31d20
cocosh0711 <cocosh0711@users.noreply.github.com>
2020-11-15 16:11:23 +0900

    setting_firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4GwISOvQXkTGiOIaZhelRS6t1Xd9vZQE",
+  authDomain: "r-review-71a3d.firebaseapp.com",
+  databaseURL: "https://r-review-71a3d.firebaseio.com",
+  projectId: "r-review-71a3d",
+  storageBucket: "r-review-71a3d.appspot.com",
+  messagingSenderId: "124484454743",
+  appId: "1:124484454743:web:b94283a814c907837e1612",
+  measurementId: "G-G8RQM94SJS"
+};
+firebase.initializeApp(firebaseConfig);
